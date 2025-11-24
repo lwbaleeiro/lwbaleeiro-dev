5e93918f2747a5dc049ba039418de715874358c5
Luan Baleeiro <dev.baleeiro@gmail.com>
2025-11-24 18:49:51 -0300

    Add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lwbaleeiro.github.io',
-  base: '/lwbaleeiro-dev',
+  base: '/',
   i18n: {
     defaultLocale: 'pt-br',
     locales: ['pt-br', 'en'],
